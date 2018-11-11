c5b82f6bac5da37478ccc75d0d5f221505ddd6d7
SnoepTablet <44743003+SnoepTablet@users.noreply.github.com>
2018-11-11 16:43:12 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+bot.on("ready", function() {
+
+  console.log("De WolfTopia is klaar voor gebruik!");
+
+});
+
+bot.on("message", function(message) {
+
+  if (message.author.equals(bot.user)) return;
+
+  if (!message.content.startsWith(botconfig.prefix)) return;
+
+  var args = message.content.substring(botconfig.prefix.length).split(" ");
+
+  switch (args[0].toLowerCase()) {
+
+    case "ip":
+      message.channel.send("Hallo " + user + "! Het IP van de server is: /n/**WolfTopia.g-s.nu");
+      break;
+    default:
+      message.channel.sendMessage("Sorry, Dit command kennen wij niet.."); 
+  }
+
+});
+
+bot.login(process.env.BOT_TOKEN);
